880db8bdb47448be0634feec031e9f8aa249942e
MrChim's <mrchimky@gmail.com>
2023-07-01 19:33:55 +0700

    update new version

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Voucher = void 0;
 const undici_1 = require("undici");
 const Voucher = (mobile, voucher) => __awaiter(void 0, void 0, void 0, function* () {
-    let roundHash = [10, 18];
+    let roundHash = [10, 35];
     if ((mobile === "" || voucher === ""))
         throw Error("Enter Mobile and Voucher");
     if (mobile.length !== roundHash[0] || mobile.match(/\D/))
@@ -36,27 +36,4 @@ const Voucher = (mobile, voucher) => __awaiter(void 0, void 0, void 0, function*
     throw Error(data.status.message);
 });
 exports.Voucher = Voucher;
-module.exports = (mobile, voucher) => __awaiter(void 0, void 0, void 0, function* () {
-    let roundHash = [10, 18];
-    if ((mobile === "" || voucher === ""))
-        throw Error("Enter Mobile and Voucher");
-    if (mobile.length !== roundHash[0] || mobile.match(/\D/))
-        throw Error("invaild_number_phone");
-    let hash = voucher.split("v=");
-    let voucher_hash = (hash[1] || hash[0]).match(/[0-9A-Za-z]+/);
-    if (roundHash[1] !== voucher_hash[0].length)
-        throw Error("invaild_voucher");
-    const data = yield (0, undici_1.fetch)(`https://gift.truemoney.com/campaign/vouchers/${voucher_hash[0]}/redeem`, {
-        method: "post",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({ mobile, voucher_hash: voucher_hash[0] })
-    }).then((response) => response.json());
-    if (data.status.message === "success") {
-        return {
-            status: "success",
-            amount: data.data.my_ticket.amount_baht,
-            hash: voucher_hash[0],
-        };
-    }
-    throw Error(data.status.message);
-});
+module.exports = exports.Voucher;
